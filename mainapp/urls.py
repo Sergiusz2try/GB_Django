c5d89bc6f00aa6e51497e9b84ee4d25ba3ab20ca
Sergiusz <kuchko.02@gmail.com>
2022-09-27 14:21:33 +0300

    Templates django

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import *
+from mainapp.apps import MainappConfig
+
+app_name = MainappConfig.name
 
 urlpatterns = [
-    path('', IndexPage.as_view()),
-    path('login/', LoginPage.as_view()),
-    path('courses/', CoursesPage.as_view()),
-    path('doc/', DocPage.as_view()),
-    path('contacts/', ContactsPage.as_view()),
-    path('news/', NewsPage.as_view()),
+    path('', IndexPage.as_view(), name='index'),
+    path('login/', LoginPage.as_view(), name='login'),
+    path('courses/', CoursesPage.as_view(), name='courses'),
+    path('doc/', DocPage.as_view(), name='docs'),
+    path('contacts/', ContactsPage.as_view(), name='contacts'),
+    path('news/', NewsPage.as_view(), name='news'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -21,9 +23,20 @@ class LoginPage(TemplateView):
 class NewsPage(TemplateView):
     template_name = 'news.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['news_title'] = 'Новость'
+        context['description'] = 'Предварительное описание новости'
+        context['news_time'] = datetime.now()
+        context['range'] = range(5)
+
+        return context
+
 
 class CoursesPage(TemplateView):
     template_name = 'courses_list.html'
+
 
 class IndexPage(TemplateView):
     template_name = 'index.html'
